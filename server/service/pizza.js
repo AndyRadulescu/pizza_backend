374ac67c1f955425c7c8c17ccef82c52883abc14
AndyRadulescu <radulescu.eduard.andrei@gmail.com>
2018-12-10 13:58:32 +0200

    send info status code to frontend

--- server/service/pizza.js
+++ server/service/pizza.js
@@ -2,9 +2,14 @@ const Pizza = require('../persistance/models').pizza;
 const Souce = require('../persistance/models').souce;
 const PizzaRepository = require('../persistance/repository/pizza-repository');
 
-exports.getSomething = (req, res) => {
+exports.getSomething = async (req, res) => {
     let pizzaRepository = new PizzaRepository(req.body.pizzas[0]);
-    res.send(pizzaRepository.insertPizzaAndSouces());
+    let response = await pizzaRepository.insertPizzaAndSouces();
+    if (response.ok === true) {
+        res.status(200).send("worked");
+    } else {
+        res.status(401).send("error");
+    }
 };
 
 exports.getAllPizza = (req, res) => {
